e8fbfe1673d2686c30f45f113ff81afefb75062d
kazimir70rus <kazzzimir@gmail.com>
2020-02-27 09:51:54 +0700

    корректировка

---
+++ models/task.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ssp\models;
+
+Class Task
+{
+    private $db;
+
+    function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    function add($executor, $task, $id_author, $data_beg, $data_end)
+    {
+        $query = 'insert into tasks (id_executor, name, id_author, data_begin, data_end) values(:id_executor, :name, :author, :data_begin, :data_end)';
+
+        return $this
+                    ->db
+                    ->insertData($query, ['id_executor' => $executor, 'name' => $task, 'author' => $id_author, 'data_begin' => $data_beg, 'data_end' => $data_end]);
+    }
+
+    function getList($id_executor)
+    {
+        $query = 'select id_task, name, id_author from tasks where id_executor = :id_executor';
+
+        return $this
+                    ->db
+                    ->getList($query, ['id_executor' => $id_executor]);
+    }
+
+    function getListAuthorTasks($id_author)
+    {
+        $query = 'select tasks.id_task, tasks.name, users.name as fio_executor, tasks.data_end from tasks, users
+                    where tasks.id_executor=users.id_user and tasks.id_author = :id_author
+                    order by tasks.data_end';
+        
+        return $this
+                    ->db
+                    ->getList($query, ['id_author' => $id_author]);
+    }
+}
---
+++ models/user.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace ssp\models;
+
+Class User
+{
+    private $db;
+
+    private $id_user;
+
+    public $name;
+
+    function __construct($db, $id_user = 0, $position = '')
+    {
+        $this->db = $db;
+
+        if ($id_user) {
+            $this->id_user = $id_user;
+            $result = $this->getInfo($id_user);
+            $this->$name = $result['name'];
+        }
+    }
+
+    function getInfo($id_user)
+    {
+        $query = 'select name from users where id_user = :id_user';
+
+        return $this
+                    ->db
+                    ->getRow($query, ['id_user' => $id_user]);
+    }
+
+    function check($login, $pass)
+    {
+        $query ='select id_user, position from users where name = :login and pass = password(:pass)';
+
+        return $this
+                    ->db
+                    ->getRow($query, ['login' => $login, 'pass' => $pass]);
+    }
+
+    function getList()
+    {
+        $query ='select id_user, name from users order by name';
+
+        return $this
+                    ->db
+                    ->getList($query);
+    }
+
+}
